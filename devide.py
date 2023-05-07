029105fbe06120fce0dcda7d9a3161b8428ed1fc
CaoY123456 <caoy_6666@126.com>
2023-05-07 15:46:59 +0800

    预处理和切割函数优化调整2

--- devide.py
+++ devide.py
@@ -1,161 +1,135 @@
 import sys
-
 import cv2
 import numpy as np
 import os
 import argparse
 import matplotlib.pyplot as plt
+import shutil
+
 plt.style.use('seaborn')
 
 # 对预处理后的二值图片进行字符的分割
-
 # 标准化后的每个字符图片的大小：
-NORMAL_IMAGE_SIZE = (32, 32)
+NORMAL_IMAGE_SIZE = (62, 64)
 
-def visualize_projection(projection):
-    plt.plot(projection)
-    plt.xlabel("Index")
-    plt.ylabel("Projection Value")
-    plt.show()
+def filter_contours(contours, binary_img, min_aspect_ratio=0.3, max_aspect_ratio=3.0, max_width_ratio=1 / 10,
+                    max_height_ratio=0.88, min_width_ratio=1 / 50, min_height_ratio=1 / 10):
+    filtered_contours = []
+    img_height, img_width = binary_img.shape
 
-# 计算图像在指定轴上的投影。它接收一个二值化的图像和一个轴参数。返回在指定轴上的投影。
-# 当axis=0时，计算垂直投影；当axis=1时，计算水平投影。
-def projection(img, axis=0):
-    return np.sum(img > 128, axis=axis)
+    max_width = img_width * max_width_ratio
+    max_height = img_height * max_height_ratio
+    min_width = img_width * min_width_ratio
+    min_height = img_height * min_height_ratio
 
-# 找到投影中的谷值，即字符间的空隙。这个函数接受投影数据，以及最小宽度和最小深度作为参数。
-# 最小宽度和最小深度用于筛选谷值，确保找到的谷值是字符之间的有效空隙。
-def find_valleys(projection, min_width=5, min_depth=15):
-    valleys = []
-    width = 0
-    start_idx = 0
-    for idx, value in enumerate(projection):
-        if value < min_depth:
-            if width == 0:
-                start_idx = idx
-            width += 1
-        else:
-            if width >= min_width:
-                valleys.append((start_idx, idx))
-            width = 0
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        aspect_ratio = w / h
 
-    # 添加最后一个区间
-    valleys.append((start_idx, len(projection)))
+        # 根据宽高比和宽高像素数量过滤轮廓
+        if min_aspect_ratio <= aspect_ratio <= max_aspect_ratio and min_width <= w <= max_width and min_height <= h <= max_height:
+            filtered_contours.append(contour)
 
-    return valleys
+    return filtered_contours
 
+def find_contours(binary_img):
+    contours, _ = cv2.findContours(binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    sorted_contours = sorted(contours, key=lambda ctr: (cv2.boundingRect(ctr)[0], cv2.boundingRect(ctr)[1]))
+    return sorted_contours
 
-# 根据谷值分割图像。这个函数接收一个图像和谷值列表，然后根据谷值将图像分割成多个部分。
-def split_lines(image, valleys, flag=0):
-    lines = []
-    if len(valleys) > 1:
-        for i in range(len(valleys) - 1):
-            start = valleys[i][1]
-            end = valleys[i + 1][0]
-            if flag == 0:
-                lines.append(image[:, start:end])
-            else:
-                lines.append(image[start:end, :])
-    return lines
+import cv2
 
+# def find_contours(binary_img, height_threshold=0.9, width_threshold=0.2):
+#     contours, _ = cv2.findContours(binary_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#
+#     refined_contours = []
+#     for contour in contours:
+#         x, y, w, h = cv2.boundingRect(contour)
+#
+#         img_height, img_width = binary_img.shape[:2]
+#
+#         if (h / img_height > height_threshold) or (w / img_width > width_threshold):
+#             # 在大轮廓中寻找小轮廓
+#             roi = binary_img[y:y + h, x:x + w]
+#             small_contours, _ = cv2.findContours(roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#
+#             for small_contour in small_contours:
+#                 refined_contours.append(small_contour)
+#         else:
+#             refined_contours.append(contour)
+#
+#     sorted_contours = sorted(refined_contours, key=lambda ctr: (cv2.boundingRect(ctr)[0], cv2.boundingRect(ctr)[1]))
+#     return sorted_contours
 
+def white_pixel_ratio(img):
+    white_pixels = np.sum(img == 255)
+    total_pixels = img.size
+    return white_pixels / total_pixels
 
-# flag为0，进行初次切割，flag为1，对单个字符进一步进行处理
-def process_image(opt):
-    binary_img_path = opt.source
-    # 读入已经二值化后的图像
+def save_chars(binary_img, contours, output_dir, binary_img_path):
+    t = 0
+
+    # 计算每个轮廓的白色像素占比和白色像素数
+    contour_ratios_and_counts = []
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        char_img = binary_img[y:y + h, x:x + w].copy()
+        ratio = white_pixel_ratio(char_img)
+        white_pixel_count = np.sum(char_img == 255)
+        contour_ratios_and_counts.append((contour, ratio, white_pixel_count, x, y))
+
+    # 按白色像素数从高到低，白色像素占比从高到低对轮廓进行排序
+    sorted_contours = sorted(contour_ratios_and_counts, key=lambda x: (x[2], x[1]), reverse=True)
+
+    # 保留前11个轮廓（如果存在的话）
+    top_contours = sorted_contours[:11]
+
+    # 按照x坐标从左到右，y坐标从上到下再排序
+    top_contours = sorted(top_contours, key=lambda x: (x[3], x[4]))
+
+    for contour, ratio, _, x, y in top_contours:
+        w, h = cv2.boundingRect(contour)[2:]
+
+        char_img = binary_img[y:y + h, x:x + w].copy()
+
+        char_img = cv2.resize(char_img, NORMAL_IMAGE_SIZE)
+
+        char_dir = os.path.join(output_dir, chr(97 + t))
+        if not os.path.exists(char_dir):
+            os.mkdir(char_dir)
+
+        file_path = os.path.join(char_dir, f"{os.path.splitext(os.path.basename(binary_img_path))[0]}{t}.jpg")
+        t += 1
+
+        cv2.imwrite(file_path, char_img)
+        print(f"保存分割后的图片【{file_path}】成功！")
+
+def process_image(binary_img_path):
     binary_img = cv2.imread(binary_img_path, cv2.IMREAD_GRAYSCALE)
-
-    # 获取不包含扩展名的图像名，以作为后面建文件夹所用
     filename_without_ext = os.path.splitext(os.path.basename(binary_img_path))[0]
 
-    # 数组vertical_projection、horizontal_projection的每个像素值为沿鸽子方向上的白色像素的数量
-    # 计算垂直投影，是一个一维数组，其反映了从上到下像素分布的情况，大小为从左到右的像素数
-    vertical_projection = projection(binary_img, axis=0)
-    # 计算水平投影，是一个一维数组，其反映了从左到右像素分布的情况，大小为从上到下的像素数
-    horizontal_projection = projection(binary_img, axis=1)
+    output_dir = f'./singledigit/{filename_without_ext}'
 
-    # 水平方向上字符的间隔数组，是一个元祖对数组，每一个元祖的第一个数表示间隔的开始，第二个数表示间隔的结束
-    vertical_valleys = find_valleys(vertical_projection)
-    horizontal_valleys = find_valleys(horizontal_projection)
+    # 删除现有的 output_dir，然后重新创建它
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
 
-    # 确保存储这张图片分割结果的文件夹已经建立
-    dir_name = './singledigit/' + filename_without_ext
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
+    contours = find_contours(binary_img)
+    # 过滤轮廓
+    filtered_contours = filter_contours(contours, binary_img)
+    save_chars(binary_img, filtered_contours, output_dir, binary_img_path)
 
-    # 多行字符情况
-    # 这里的lines每行图片的数组
-    lines = split_lines(binary_img, horizontal_valleys, 1)
-    t = 0
-    for line in lines:
-        # 对每行进行垂直投影
-        line_projection = projection(line, axis=0)
-        # 计算每行的垂直谷值
-        line_vertical_valleys = find_valleys(line_projection)
-        # 使用垂直谷值将字符分割
-        chars = split_lines(line, line_vertical_valleys)
-        for char_img in chars:
-            # 保存分割后的字符图片
-            tmp_dir_name = dir_name + '/' + chr(97 + t)
-
-            if not os.path.exists(tmp_dir_name):
-                os.mkdir(tmp_dir_name)
-
-            filePath = tmp_dir_name + '/' + filename_without_ext + str(t) + '.jpg'
-            t = t + 1
-
-            if not char_img is None:
-                # 保存前将图片的分辨率重置为20 * 20
-                # char_img_r = cv2.resize(char_img, (20, 20))
-                writeResult = cv2.imwrite(filePath, char_img)
-                if writeResult:
-                    print('保存分割后的图片【' + filePath + '】成功！')
-                else:
-                    print('保存分割后的图片【' + filePath + '】失败！')
-
-        return dir_name
-
-def narmalize_process(single_image_dir):
-    for image_dir in os.listdir(single_image_dir):
-        image_dir = single_image_dir + '/' + image_dir + '/'
-        for image_path in os.listdir(image_dir):
-            image_path = image_dir + image_path
-            if image_path.endswith('.jpg'):
-                image_path = str(image_path)
-                img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-                # 调整图像大小为 NORMAL_IMAGE_SIZE(x * y)
-                # img = cv2.resize(img, NORMAL_IMAGE_SIZE, interpolation=cv2.INTER_AREA)
-
-                # 归一化操作
-                # 统计每行水平方向上的白色像素数
-                row_white_pixel_count = np.sum(img == 255, axis=1)
-
-                # 统计每列垂直方向上的白色像素数
-                col_white_pixel_count = np.sum(img == 255, axis=0)
-
-                # 获取字符的高度和宽度
-                # char_height = np.where(row_white_pixel_count > 15)[0][-1] - np.where(row_white_pixel_count > 0)[0][0] + 1
-                # char_width = np.where(col_white_pixel_count > 15)[0][-1] - np.where(col_white_pixel_count > 0)[0][0] + 1
-
-                # 裁剪出字符部分
-                img_cropped = img[
-                              np.where(row_white_pixel_count > 15)[0][0]:np.where(row_white_pixel_count > 0)[0][-1] + 1,
-                              np.where(col_white_pixel_count > 15)[0][0]:np.where(col_white_pixel_count > 0)[0][-1] + 1]
-
-                img = cv2.resize(img_cropped, NORMAL_IMAGE_SIZE, interpolation=cv2.INTER_AREA)
-                cv2.imwrite(image_path, img)
+    return output_dir
 
 def run_devide(opt):
-    single_image_dir = process_image(opt)
+    single_image_dir = process_image(opt.source)
     print('===============================初步切割结束===============================')
-    narmalize_process(single_image_dir)
-    print('===============================尺寸归一化结束===============================')
     return single_image_dir
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source', type=str, default='binary_images/IMG_0155_0.jpg', help='picture file')
+    parser.add_argument('--source', type=str, default='binary_images/IMG_0164_0.jpg', help='picture file')
     opt = parser.parse_args()
     run_devide(opt)
     sys.exit()
--- pretreat.py
+++ pretreat.py
@@ -7,6 +7,13 @@ import numpy as np
 
 # 预处理脚本
 
+def set_border_black(binary_img, border_size):
+    height, width = binary_img.shape
+    binary_img[:border_size, :] = 0
+    binary_img[height-border_size:, :] = 0
+    binary_img[:, :border_size] = 0
+    binary_img[:, width-border_size:] = 0
+
 def contrast_stretching(img, low_percent=1, high_percent=99):
     """
     对比度拉伸
@@ -30,31 +37,7 @@ def run_pretreat(opt):
     image_path = opt.source
     img = cv2.imread(image_path)
 
-    # 将彩色图像转换为灰度图像
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # 对比度拉伸
-    stretched_img = contrast_stretching(gray_img)
-
-    # 3. 高斯滤波
-    gaussian_blur_img = cv2.GaussianBlur(stretched_img, (3, 3), 0)
-
-    # 4. 使用 Otsu's 二值化 - 这里的二值化很容易出意外，应当根据不同的图像特征选择合适的二值化策略
-    ret, binary_img1 = cv2.threshold(gaussian_blur_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    ret, binary_img2 = cv2.threshold(gaussian_blur_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-    # 5. 选择平均像素值较低的图像
-    if np.mean(binary_img1) < np.mean(binary_img2):
-        binary_img = binary_img1
-    else:
-        binary_img = binary_img2
-
-    # 6. 对二值图像执行开运算
-    kernel = np.ones((1, 1), np.uint8)
-    opened_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
-
-    img = opened_img
-    width, height = img.shape
+    width, height = img.shape[:2]
 
     maxNum = width
     minNum = height
@@ -70,9 +53,50 @@ def run_pretreat(opt):
         else:
             img = cv2.resize(img, (randNum, int(randNum / rate)))
 
+    # 将彩色图像转换为灰度图像
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 对比度拉伸
+    stretched_img = contrast_stretching(gray_img)
+    # equalized_img = cv2.equalizeHist(stretched_img)
+
+    # 3. 高斯滤波
+    # gaussian_blur_img = cv2.GaussianBlur(stretched_img, (3, 3), 0)
+
+    # 双边滤波
+    # filtered_img = cv2.bilateralFilter(equalized_img, 9, 75, 75)
+
+    binary_img = cv2.adaptiveThreshold(stretched_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 31, 10)
+
+    # 6. 对二值图像执行开运算
+    kernel = np.ones((3, 3), np.uint8)
+    opened_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
+
+    # 5. 对二值图像执行闭运算，填补间断的白线
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    closed_img = cv2.morphologyEx(opened_img, cv2.MORPH_CLOSE, kernel)
+
+    # 统计二值化图像中黑白像素点的数量比例
+    black_pixels = np.sum(closed_img == 0)
+    white_pixels = np.sum(closed_img == 255)
+    ratio = black_pixels / white_pixels
+
+    # 7. 根据比例确定二值化后的前景和背景颜色
+    if ratio > 0.5:
+        foreground_color = 0
+        background_color = 255
+    else:
+        foreground_color = 255
+        background_color = 0
+
+    img = closed_img
+
+    # 7. 去除外边缘的白色框
+    set_border_black(img, border_size=5)
+
     image_name = os.path.splitext(os.path.basename(image_path))[0]
     filename = f"{pre_treat_images_dir}/{image_name}.jpg"
-    writeResult = cv2.imwrite(filename, binary_img)
+    writeResult = cv2.imwrite(filename, img)
     if writeResult:
         print("存储预处理后的图片【" + filename + "】存储成功！")
     else:
@@ -82,6 +106,6 @@ def run_pretreat(opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source', type=str, default='cropped_images/IMG_0157_0.jpg', help='picture file')
+    parser.add_argument('--source', type=str, default='cropped_images/IMG_0164_0.jpg', help='picture file')
     opt = parser.parse_args()
     run_pretreat(opt)
